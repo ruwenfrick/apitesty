6d881c560d310e6d2bf9ff4c3b0cfe704476de6e
Ruwen Frick <ruwen.frick@aviita.li>
2022-08-24 13:43:02 +0200

    add files endpoint

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from datetime import datetime
 import re
+import os.path as path
 
 app = Flask(__name__)
 
@@ -13,8 +14,6 @@ def hello_there(name):
     now = datetime.now()
     formatted_now = now.strftime("%A, %d %B, %Y at %X")
 
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
     match_object = re.match("[a-zA-Z]+", name)
 
     if match_object:
@@ -24,6 +23,16 @@ def hello_there(name):
 
     content = "Hello there, " + clean_name + "! It's " + formatted_now
     return content
+
+@app.route("/files/<code>")
+def getFile(code):
+    match_object = re.match("^[0-9]*$", code)
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "default.txt"
+    file = path.join("files", f"{clean_name}.json")
+    return Flask.send_file(file)
     
 if __name__ == "__main__":
     app.run()
