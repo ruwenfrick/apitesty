baa8a4890f47ac377610954ebc8abf844cdb930a
ruwenfrick <111970339+ruwenfrick@users.noreply.github.com>
2022-08-25 17:47:32 +0200

    allow for ip to be passed as argument

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+import sys
 
 def get_file(url):
     response = requests.get(url)
@@ -12,5 +13,6 @@ def write_file(to_write, path):
         json.dump(to_write, file)
 
 if __name__ == "__main__":
-    to_write = get_file("https://apitesty.azurewebsites.net/files/1")
+    ip = sys.argv[1]
+    to_write = get_file(f"http://{ip}/files/1")
     write_file(to_write, os.path.join("rec_files", "1.json"))
