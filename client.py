b8120aa01d47fb8521307bd67385df43233413c4
Ruwen Frick <ruwen.frick@aviita.li>
2022-08-25 10:21:01 +0200

    add create dir if not exists feature

--- client.py
+++ client.py
@@ -1,15 +1,16 @@
 import requests
 import json
-import os.path as path
+import os
 
 def get_file(url):
     response = requests.get(url)
     return response.json()
 
 def write_file(to_write, path):
+    os.makedirs(os.path.dirname(path), exist_ok=True)
     with open(path, "w+") as file:
         json.dump(to_write, file)
 
 if __name__ == "__main__":
     to_write = get_file("https://apitesty.azurewebsites.net/files/1")
-    write_file(to_write, path.join("rec_files", "1.json"))
+    write_file(to_write, os.path.join("rec_files", "1.json"))
